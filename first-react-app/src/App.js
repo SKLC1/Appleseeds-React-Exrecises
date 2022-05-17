4be43aedd6c5e5e4510120fd091d6b546f6bb95e
SKLC1 <segevklein2000@gmail.com>
2022-05-17 21:19:39 +0300

    7.1-7.3

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -8,45 +8,42 @@ import { useState } from 'react';
 function App(){
   return(
     <div>
-      <Box/>
+      <Timer/>
     </div>
   )
 }
-class Box extends React.Component{
+class Timer extends React.Component{
   constructor() {
     super()
-    this.state = {
-      back: 'blue',
-      counter: 0,
-      rad: 0,
-    }
+    this.state = {timerFinished: false}
   }
   componentDidMount() {
-    let randomColor = Math.floor(Math.random()*16777215).toString(16);
-      setTimeout(()=>{
-      this.setState(()=>{return{back: `#${randomColor}`}})
-    },500)
+    setTimeout((prevState) => {
+      this.setState(()=>{return {timerFinished: true}})
+      console.log('a');
+    }, 3000);
   }
-  componentDidUpdate(){
-    let randomColor = Math.floor(Math.random()*16777215).toString(16);
-      setTimeout(()=>{
-      this.setState(()=>{return{back: `#${randomColor}`,counter: this.state.counter + 1 }})
-      console.log('updated');
-      console.log(this.state.counter);
-      if (this.state.counter === 5) {
-        {this.state.rad = '50%'}
-        console.log(this.state.rad);
-      }
-    },500)
+  renderContent() {
+    if (!this.state.timerFinished) {
+      return(
+        <div>
+          <div id="loading"></div>
+        </div>
+      )
+    } else {
+      return(
+        <div>
+          <div>timerFinished</div>
+        </div>
+      )
+    }
   }
   render() {
     return(
       <div>
-        <div className='box8' style={{backgroundColor: this.state.back, borderRadius: this.state.rad}}>
-        </div>
+       {this.renderContent()}
       </div>
-    )
+      )
   }
 }
-
 export default App
--- first-react-app/src/components/Exrecise 8.3.jsx
+++ first-react-app/src/components/Exrecise 8.3.jsx
@@ -1,3 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import { useState } from 'react';
+
+
+
 function App(){
   return(
     <div>
@@ -9,15 +16,33 @@ class Box extends React.Component{
   constructor() {
     super()
     this.state = {
-      // back: `rgb(${Math.floor(Math.random *120)},${Math.floor(Math.random *120)},${Math.floor(Math.random *120)})`,
-      back: 'red',
+      back: 'blue',
+      counter: 0,
+      rad: 0,
     }
   }
-
+  componentDidMount() {
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+      setTimeout(()=>{
+      this.setState(()=>{return{back: `#${randomColor}`}})
+    },500)
+  }
+  componentDidUpdate(){
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+      setTimeout(()=>{
+      this.setState(()=>{return{back: `#${randomColor}`,counter: this.state.counter + 1 }})
+      console.log('updated');
+      console.log(this.state.counter);
+      if (this.state.counter === 5) {
+        {this.state.rad = '50%'}
+        console.log(this.state.rad);
+      }
+    },500)
+  }
   render() {
     return(
       <div>
-        <div className='box' style={{backgroundColor: this.state.back}}>
+        <div className='box8' style={{backgroundColor: this.state.back, borderRadius: this.state.rad}}>
         </div>
       </div>
     )
