193a9efc73295d294051db3dab3ac4c4586d9b1b
SKLC1 <segevklein2000@gmail.com>
2022-05-18 16:02:05 +0300

    save

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,49 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { useState } from 'react';
+// import { useState } from 'react';
 
-
-
-function App(){
-  return(
-    <div>
-      <Timer/>
-    </div>
-  )
-}
-class Timer extends React.Component{
+class App extends React.Component{
   constructor() {
     super()
-    this.state = {timerFinished: false}
-  }
-  componentDidMount() {
-    setTimeout((prevState) => {
-      this.setState(()=>{return {timerFinished: true}})
-      console.log('a');
-    }, 3000);
-  }
-  renderContent() {
-    if (!this.state.timerFinished) {
-      return(
-        <div>
-          <div id="loading"></div>
-        </div>
-      )
-    } else {
-      return(
-        <div>
-          <div>timerFinished</div>
-        </div>
-      )
+    this.state = {
+      res: null,
+      colors: ["yellow","red","blue"],
     }
   }
+  assignColors() {
+    const colorRes = this.state.colors.map(colorProp => {
+      console.log(colorProp);
+      return <ColorBtn color={colorProp} changeColor={this.changeColor}/>
+    });
+    return colorRes
+  }
+  changeColor(color){
+    console.log(color);
+    console.log(this);
+    this.setState((prevState)=>{return {res: color}})
+  }
+  render() {
+    return (
+      <div>
+        {this.assignColors()}
+       <div>{this.state.res}</div>
+      </div>
+    )
+  }
+}
+class ColorBtn extends React.Component{
+
   render() {
     return(
       <div>
-       {this.renderContent()}
+       <button onClick={()=>this.props.changeColor(this.props.color)}
+        style={{backgroundColor: this.props.color}}>
+         {this.props.color}
+       </button>
       </div>
-      )
+    )
   }
 }
 export default App
---
+++ react-exrecises/my-app/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+
+
+
+function App() {
+  return (
+    <div>
+      <Colors/>
+    </div>
+  );
+}
+
+class Colors extends React.Component {
+  render() {
+    return(
+      <div>Test</div>
+    )
+  }
+
+}
+export default App;
