8f4af6ed92f5b1b5245e73e5d3ac3b3d95a95118
SKLC1 <segevklein2000@gmail.com>
2022-05-18 20:03:01 +0300

    added exr

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -5,40 +5,74 @@ import React from 'react';
 class App extends React.Component{
   constructor() {
     super()
-    this.state = {
-      res: null,
-      colors: ["yellow","red","blue"],
-    }
-  }
-  changeColor=(color)=>{
-    this.setState(()=>{return {res: color}})
-  }
-  assignColors() {
-    const colorRes = this.state.colors.map(colorProp => {
-      return <ColorBtn color={colorProp} changeFunc={this.changeColor}/>
-    });
-    return colorRes
+    this.state = {}
   }
   render() {
-    return (
-      <div>
-        {this.assignColors()}
-       <div>{this.state.res}</div>
-      </div>
+    return(
+      <>
+      <Create/>
+      </>
     )
   }
 }
-class ColorBtn extends React.Component{
+
+class Create extends React.Component{
+  constructor() {
+    super()
+    this.state = {
+      firstName: null,
+      lastName: null,
+      age: null,
+      text: null,
+      review: false,
+    }
+  }
+  setFirst=(value)=>{
+    this.setState({ firstName: value })
+  }
+  setLast=(value)=>{
+    this.setState({ lastName: value })
+  }
+  setSelect=(value)=>{
+    this.setState({ age: value })
+  }
+  setText=(value)=>{
+    this.setState({ text: value })
+  }
+  reviewForm=()=>{
+    this.setState({ review: true })
+  }
 
   render() {
+    if(!this.state.review) {
     return(
-      <div>
-       <button onClick={()=>this.props.changeFunc(this.props.color)}
-        style={{backgroundColor: this.props.color}}>
-         {this.props.color}
-       </button>
+      <div className='form-box'>
+       <form>
+        <label>First Name</label>
+        <input type="text" onChange={(e)=> this.setFirst(e.target.value)}/>
+        <label>Last Name</label>
+        <input type="text" onChange={(e)=> this.setLast(e.target.value)}/>
+        <select onChange={(e)=> this.setSelect(e.target.value)}>
+          <option value="0-15">0-15</option>
+          <option value="15-30">15-30</option>
+        </select>
+        <label>Free text</label>
+        <input onChange={(e)=> this.setText(e.target.value)} type="text"/>
+        <button type='button' onClick={()=> this.reviewForm()}>Continue</button>
+       </form>
       </div>
     )
+  } else {
+    return(
+      <div>
+        <p>{this.state.firstName}</p>
+        <p>{this.state.lastName}</p>
+        <p>{this.state.age}</p>
+        <p>{this.state.text}</p>
+        <button type='submit'>Submit</button>
+      </div>
+      )
+    }
   }
 }
 export default App
--- first-react-app/src/components/Exrecise11.2.jsx
+++ first-react-app/src/components/Exrecise11.2.jsx
@@ -1,2 +1,29 @@
 
 
+import './App.css';
+import React from 'react';
+// import { useState } from 'react';
+
+class App extends React.Component{
+  constructor() {
+    super()
+    this.state = {}
+  }
+  render() {
+    return(
+      <>
+       <CheckBox/>
+       <CheckBox/>
+       <CheckBox/>
+      </>
+    )
+  }
+}
+
+class CheckBox extends React.Component{
+  render() {
+    return(
+      <input type="checkbox">{text}</input>
+    )
+  }
+}
