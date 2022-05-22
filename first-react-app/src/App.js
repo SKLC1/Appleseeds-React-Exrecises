ad6e8393c351b90f53711996e586166c6fb69e70
SKLC1 <segevklein2000@gmail.com>
2022-05-22 18:50:12 +0300

    added exrecises

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -42,14 +42,19 @@ class FetchAvatars extends React.Component{
 class MapAvatars extends React.Component {
   constructor() {
     super()
-    this.state = {didUpdate: false}
+    this.state = {didUpdate: false, mappedData: []}
   }
   getRelevantData = ()=>{
-    this.state.avatars.map((person,idx)=>{
-      this.setState({
-        person: {name: person.name.first}
-      })
+    let newMappedData = this.props.avatars.map((person)=> {
+      return {person: {
+        first: person.name.first,
+        last: person.name.last,       
+        id: person.phone,       
+        img: person.picture,       
+      }}
     })
+    console.log(newMappedData)
+    this.setState({mappedData: newMappedData})
   }
   async componentDidUpdate() {
     if(!this.state.didUpdate){
@@ -61,9 +66,21 @@ class MapAvatars extends React.Component {
     return(
     <>
       <div>
+        <DisplayAvatars mappedData={this.state.mappedData}this/>
       </div>
     </>
   )
   }
+}
+ function DisplayAvatars(props) {
+   function appendAvatar() {
+     props.mappedData.map((person)=> {
+       return <div>asd</div>
+     })
+   }
+
+ return(
+   appendAvatar()
+ )
 }
 export default App
