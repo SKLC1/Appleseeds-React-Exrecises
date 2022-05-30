e8acf9bbcbf64b14f284fba0469dbb7400df1fcc
SKLC1 <segevklein2000@gmail.com>
2022-05-30 12:57:18 +0300

    19.2

--- more-react-exrecises/src/componenets/Exrecise19.2/Exrecise.19.2.jsx
+++ more-react-exrecises/src/componenets/Exrecise19.2/Exrecise.19.2.jsx
@@ -1,24 +1,40 @@
+import axios from 'axios'
+import {useState, useEffect } from 'react'
 
 
 function Exrecise192() {
 
-  const [search,setSearch] = useState({query: 'hooks'})
+  const [countries,setCountries] = useState({res: [] })
+  const [search,setSearch] = useState({input: ''})
   
   useEffect(()=>{getSearch()},[])
   
   async function getSearch() {
     try{
-      const {data} = await axios.get(`https://hn.algolia.com/api/v1/search?query=${search.query}`)
+      const {data} = await axios.get(`https://restcountries.com/v3.1/all`)
       console.log(data)
+      setCountries({res: data})
     } catch (e){
       console.log(e);
     }
   }
+  function insertCountries(){
+    return countries.res.map((country,idx)=>{
+      return <div key={idx}>{country.name.common}</div>
+    })
+  }
+  useEffect(()=>{filterSearch()},[search])
+
+   function filterSearch() {
+    console.log(search.input)
+    const newRes = countries.res.filter(country=> country.name.common.includes(search.input));
+    setCountries({res: newRes})
+  }
   return ( 
     <div>
-      <input/>
+      <input onChange={(e)=>{setSearch({input: e.target.value})}}/>
       <div>
-        
+        {insertCountries()}
       </div>
     </div>
    );
